af058f0686dddaffcc3672f1c1d25c23d6c82e35
Eric Hodel <drbrain@segment7.net>
2022-07-11 17:16:09 -0700

    Disable lightbulb in number column
    Code actions are already marked on the right of the line

--- lspsaga.lua
+++ lspsaga.lua
@@ -3,4 +3,10 @@ local saga = require 'lspsaga'
 saga.init_lsp_saga({
   border_style = "rounded",
   diagnostic_header = { "❌", "⚠️", "ℹ️", "💡" },
+  code_action_lightbulb = {
+    enable = true,
+    sign = false,
+    sign_priority = 0,
+    virtual_text = true,
+  },
 })
