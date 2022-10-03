78a454570dded1ce13a1d1e2433d107d436f5432
Eric Hodel <drbrain@segment7.net>
2022-10-03 14:04:05 -0700

    Fix indentation

--- lsp.lua
+++ lsp.lua
@@ -13,10 +13,11 @@ local lsp_flags = {
 }
 
 require('lspconfig')['rust_analyzer'].setup {
-    on_attach = on_attach,
-    flags = lsp_flags,
-    -- Server-specific settings...
-    settings = {
-      ["rust-analyzer"] = {}
-    }
+  -- cmd = { "rustup", "run", "stable", "rust-analyzer" },
+  on_attach = on_attach,
+  flags = lsp_flags,
+  -- Server-specific settings...
+  settings = {
+    ["rust-analyzer"] = {}
+  }
 }
