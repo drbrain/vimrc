b5a3c753088d5cdaf858f08058233c5474bab197
Eric Hodel <drbrain@segment7.net>
2022-10-28 10:38:58 -0700

    diffview preferences

---
+++ diffview.lua
@@ -0,0 +1,11 @@
+require("diffview").setup({
+  enhanced_diff_hl = true,
+  view = {
+    default = {
+      layout = "diff2_horizontal",
+    },
+    merge_tool = {
+      layout = "diff4_mixed"
+    }
+  }
+})
