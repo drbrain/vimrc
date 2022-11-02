0052a03210b0589cda584dd73b8582cdc7a10bd5
Eric Hodel <drbrain@segment7.net>
2022-11-01 19:05:11 -0700

    Don't collide with <Leader>ca for code-action

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -27,24 +27,24 @@ function init(use)
 
       local opts = { noremap = true, silent = true }
 
-      vim.keymap.set("n", "<Leader>ct", crates.toggle, opts)
-      vim.keymap.set("n", "<Leader>cr", crates.reload, opts)
+      vim.keymap.set("n", "<Leader>Ct", crates.toggle, opts)
+      vim.keymap.set("n", "<Leader>Cr", crates.reload, opts)
 
-      vim.keymap.set("n", "<Leader>cv", crates.show_versions_popup, opts)
-      vim.keymap.set("n", "<Leader>cf", crates.show_features_popup, opts)
-      vim.keymap.set("n", "<Leader>cd", crates.show_dependencies_popup, opts)
+      vim.keymap.set("n", "<Leader>Cv", crates.show_versions_popup, opts)
+      vim.keymap.set("n", "<Leader>Cf", crates.show_features_popup, opts)
+      vim.keymap.set("n", "<Leader>Cd", crates.show_dependencies_popup, opts)
 
-      vim.keymap.set("n", "<Leader>cu", crates.update_crate, opts)
-      vim.keymap.set("v", "<Leader>cu", crates.update_crates, opts)
-      vim.keymap.set("n", "<Leader>ca", crates.update_all_crates, opts)
-      vim.keymap.set("n", "<Leader>cU", crates.upgrade_crate, opts)
-      vim.keymap.set("v", "<Leader>cU", crates.upgrade_crates, opts)
-      vim.keymap.set("n", "<Leader>cA", crates.upgrade_all_crates, opts)
+      vim.keymap.set("n", "<Leader>Cu", crates.update_crate, opts)
+      vim.keymap.set("v", "<Leader>Cu", crates.update_crates, opts)
+      vim.keymap.set("n", "<Leader>Ca", crates.update_all_crates, opts)
+      vim.keymap.set("n", "<Leader>CU", crates.upgrade_crate, opts)
+      vim.keymap.set("v", "<Leader>CU", crates.upgrade_crates, opts)
+      vim.keymap.set("n", "<Leader>CA", crates.upgrade_all_crates, opts)
 
-      vim.keymap.set("n", "<Leader>cH", crates.open_homepage, opts)
-      vim.keymap.set("n", "<Leader>cR", crates.open_repository, opts)
-      vim.keymap.set("n", "<Leader>cD", crates.open_documentation, opts)
-      vim.keymap.set("n", "<Leader>cC", crates.open_crates_io, opts)
+      vim.keymap.set("n", "<Leader>CH", crates.open_homepage, opts)
+      vim.keymap.set("n", "<Leader>CR", crates.open_repository, opts)
+      vim.keymap.set("n", "<Leader>CD", crates.open_documentation, opts)
+      vim.keymap.set("n", "<Leader>CC", crates.open_crates_io, opts)
     end,
 }
 end
