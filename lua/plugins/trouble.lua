fd27d143d78abcc2bb7a23a99f6221a92342060e
Eric Hodel <drbrain@segment7.net>
2022-12-05 17:14:34 -0800

    Toggle trouble on xx

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -14,7 +14,7 @@ function init(use)
 
       local opts = { silent = true, noremap = true }
 
-      vim.api.nvim_set_keymap("n", "<leader>xx", "<cmd>Trouble<cr>", opts)
+      vim.api.nvim_set_keymap("n", "<leader>xx", "<cmd>TroubleToggle<cr>", opts)
       vim.api.nvim_set_keymap("n", "<leader>xw", "<cmd>Trouble workspace_diagnostics<cr>", opts)
       vim.api.nvim_set_keymap("n", "<leader>xd", "<cmd>Trouble document_diagnostics<cr>", opts)
       vim.api.nvim_set_keymap("n", "<leader>xl", "<cmd>Trouble loclist<cr>", opts)
