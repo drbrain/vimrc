4376be33393aa3bff65abf5857579017b33e3dcf
Eric Hodel <drbrain@segment7.net>
2023-02-09 22:52:40 -0800

    Simplify

--- lua/plugins/appearance.lua
+++ lua/plugins/appearance.lua
@@ -108,13 +108,12 @@ return {
 
   {
     "norcalli/nvim-colorizer.lua",
-    config = function()
-      require("colorizer").setup({
-        "*"; -- Highlight all files, but customize some others.
-        css = { rgb_fn = true; }; -- Enable parsing rgb(...) functions in css.
-        html = { names = false; } -- Disable parsing "names" like Blue or Gray
-      })
-    end,
+
+    opts = {
+      "*"; -- Highlight all files, but customize some others.
+      css = { rgb_fn = true; }; -- Enable parsing rgb(...) functions in css.
+      html = { names = false; } -- Disable parsing "names" like Blue or Gray
+    },
   },
 
   -- We override this with nvim-material-icon, but need it for compatibility
