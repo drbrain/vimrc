ed70ff60b78b13bc8619a829d19e34811a081fc0
Eric Hodel <drbrain@segment7.net>
2023-02-11 00:00:42 -0800

    Add notify, update treesitter-textobjects

--- lua/plugins/appearance.lua
+++ lua/plugins/appearance.lua
@@ -81,6 +81,30 @@ return {
   },
 
   {
+    "rcarriga/nvim-notify",
+    keys = {
+      {
+        "<Leader>un",
+        function()
+          require("notify").dismiss({ silent = true, pending = true })
+        end,
+        desc = "Delete all Notifications",
+      },
+    },
+
+    opts = {
+      render = "compact",
+      timeout = 3000,
+      max_height = function()
+        return math.floor(vim.o.lines * 0.75)
+      end,
+      max_width = function()
+        return math.floor(vim.o.columns * 0.75)
+      end,
+    },
+  },
+
+  {
     "gelguy/wilder.nvim",
     dependencies = {
       "nvim-cmp",
