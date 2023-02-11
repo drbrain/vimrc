a1264bdbf8f872873ba201a79666d6ff23613ed3
Eric Hodel <drbrain@segment7.net>
2023-02-10 18:04:56 -0800

    Lazy load diffview

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -3,18 +3,26 @@ return {
     "sindrets/diffview.nvim",
     dependencies = "plenary.nvim",
 
-    config = function()
-      require("diffview").setup({
-        enhanced_diff_hl = true,
-        view = {
-          default = {
-            layout = "diff2_horizontal",
-          },
-          merge_tool = {
-            layout = "diff4_mixed"
-          }
+    cmd = {
+      "DiffviewLog",
+      "DiffviewOpen",
+      "DiffviewClose",
+      "DiffviewRefresh",
+      "DiffviewFocusFiles",
+      "DiffviewFileHistory",
+      "DiffviewToggleFiles",
+    },
+
+    opts = {
+      enhanced_diff_hl = true,
+      view = {
+        default = {
+          layout = "diff2_horizontal",
+        },
+        merge_tool = {
+          layout = "diff4_mixed"
         }
-      })
-    end,
+      }
+    }
   },
 }
