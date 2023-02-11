8d49eba7c63a83c39dd2fb756d4f0c993c577411
Eric Hodel <drbrain@segment7.net>
2023-02-10 22:30:22 -0800

    Remove map.lua

--- init.lua
+++ init.lua
@@ -8,6 +8,5 @@ vim.api.nvim_exec([[
 source ~/.config/nvim/autocmd.vim
 source ~/.config/nvim/cdpath.vim
 source ~/.config/nvim/highlight.vim
-source ~/.config/nvim/map.lua
 "source ~/.config/nvim/ruby-lang.vim
 ]], true)
