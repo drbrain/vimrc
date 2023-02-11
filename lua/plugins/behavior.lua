88833fc42cf62e398013782d997fc5bafed01f31
Eric Hodel <drbrain@segment7.net>
2023-02-10 16:52:05 -0800

    Make behavior plugins lazier

--- lua/plugins/behavior.lua
+++ lua/plugins/behavior.lua
@@ -1,26 +1,39 @@
 return {
-  "godlygeek/tabular",
+  {
+    "godlygeek/tabular",
+
+    cmd = {
+      "Tab",
+      "Tabularize",
+    },
+
+    keys = {
+      { "<Leader>a=", ":Tabularize /=<CR>",    mode = "n", desc = "Align on =" },
+      { "<Leader>a=", ":Tabularize /=<CR>",    mode = "v", desc = "Align on =" },
+
+      { "<Leader>ah", ":Tabularize /=><CR>",   mode = "n", desc = "Align on =>" },
+      { "<Leader>ah", ":Tabularize /=><CR>",   mode = "v", desc = "Align on =>" },
+
+      { "<Leader>a:", ":Tabularize /:\\zs<CR>", mode = "n", desc = "Align after :" },
+      { "<Leader>a:", ":Tabularize /:\\zs<CR>", mode = "v", desc = "Align after :" },
+    }
+  },
 
   {
     "junegunn/vim-easy-align",
 
-    config = function()
-      -- Start interactive EasyAlign in visual mode (e.g. vipga)
-      vim.api.nvim_set_keymap("x", "ga", "<Plug>(EasyAlign)", { silent = true })
-
-      -- Start interactive EasyAlign for a motion/text object (e.g. gaip)
-      vim.api.nvim_set_keymap("n", "ga", "<Plug>(EasyAlign)", { silent = true })
-    end
+    keys = {
+      { "ga", "<Plug>(EasyAlign)", mode = "x", desc = "Easy Align" },
+      { "ga", "<Plug>(EasyAlign)", mode = "n", desc = "Easy Align" },
+    },
   },
 
   {
     "jinh0/eyeliner.nvim",
 
-    config = function()
-      require("eyeliner").setup({
-        highlight_on_key = true
-      })
-    end
+    opts = {
+      highlight_on_key = true,
+    },
   },
 
   -- normal mode keymaps:
@@ -61,10 +74,10 @@ return {
       "nvim-ts-context-commentstring",
     },
 
-    config = function()
-        require("Comment").setup({
+    opts = function()
+      return {
         pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-      })
+      }
     end
   },
 }
