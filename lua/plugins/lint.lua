7255e272e584aa38cd1ea2396449c335ea1b667f
Eric Hodel <drbrain@segment7.net>
2024-01-11 10:34:28 -0800

    Also lint on InsertLeave

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -12,7 +12,10 @@ return {
         zsh = { "zsh", },
       }
 
-      vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+      vim.api.nvim_create_autocmd({
+        "BufWritePost",
+        "InsertLeave",
+      }, {
         callback = function()
           require("lint").try_lint()
         end,
