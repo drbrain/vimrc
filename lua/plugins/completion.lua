31ea62e8b9935eeb5e09e3db6eb59afd9c4a9278
Eric Hodel <drbrain@segment7.net>
2024-05-22 12:39:27 -0700

    Update lsp plugins

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,12 +1,29 @@
 return {
   { "L3MON4D3/LuaSnip",                     event = "VeryLazy" },
-  { "hrsh7th/cmp-nvim-lua",                 event = "VeryLazy", dependencies = "nvim-cmp" },
   { "hrsh7th/cmp-nvim-lsp",                 event = "VeryLazy", dependencies = "nvim-cmp" },
   { "hrsh7th/cmp-nvim-lsp-document-symbol", event = "VeryLazy", dependencies = "nvim-cmp" },
   { "hrsh7th/cmp-nvim-lsp-signature-help",  event = "VeryLazy", dependencies = "nvim-cmp" },
+  { "hrsh7th/cmp-nvim-lua",                 event = "VeryLazy", dependencies = "nvim-cmp" },
   { "hrsh7th/cmp-path",                     event = "VeryLazy", dependencies = "nvim-cmp" },
-  { "hrsh7th/nvim-cmp",                     event = "VeryLazy"  },
   { "onsails/lspkind.nvim",                 event = "VeryLazy"  },
   { "rafamadriz/friendly-snippets",         event = "VeryLazy"  },
   { "saadparwaiz1/cmp_luasnip",             event = "VeryLazy", dependencies = "nvim-cmp" },
+
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "zjp-CN/nvim-cmp-lsp-rs",
+    },
+    event = "VeryLazy",
+
+    opts = {
+      sources = {
+        { name = "nvim_lsp" },
+        { name = "nvim_lsp_signature_help" },
+        { name = "nvim_lsp_document_symbol" },
+        { name = "path" },
+        { name = "nvim_lua" },
+      },
+    }
+  },
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -51,27 +51,41 @@ return {
   },
 
   {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = {
+      "VonHeikemen/lsp-zero.nvim",
+    },
+
+    opts = {
+      handlers = {
+        function(server_name)
+          require("lspconfig")[server_name].setup({})
+        end
+      }
+    }
+  },
+
+  {
     "VonHeikemen/lsp-zero.nvim",
+    branch = "v3.x",
     dependencies = {
       -- LSP Support
       "neovim/nvim-lspconfig",
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
 
-      -- Autocompletion
-      "hrsh7th/nvim-cmp",
-      "hrsh7th/cmp-nvim-lsp",
-      -- "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "saadparwaiz1/cmp_luasnip",
-      "hrsh7th/cmp-nvim-lua",
-
-      -- Snippets
-      "L3MON4D3/LuaSnip",
-      "rafamadriz/friendly-snippets",
-
-      -- Language plugins
-      "simrat39/rust-tools.nvim",
+      --   -- Autocompletion
+      --   "hrsh7th/nvim-cmp",
+      --   "hrsh7th/cmp-nvim-lsp",
+      --   -- "hrsh7th/cmp-buffer",
+      --   "hrsh7th/cmp-path",
+      --   "saadparwaiz1/cmp_luasnip",
+      --   "hrsh7th/cmp-nvim-lua",
+      --
+      --   -- Snippets
+      --   "L3MON4D3/LuaSnip",
+      --   "rafamadriz/friendly-snippets",
+      --
+      --   -- Language plugins
+      --   "simrat39/rust-tools.nvim",
     },
 
     event = {
@@ -80,41 +94,44 @@ return {
     },
 
     keys = {
-      { "K",     "<Cmd>lua vim.lsp.buf.hover()<CR>", desc = "Hover" },
-      { "gd",    "<Cmd>lua vim.lsp.buf.definition()<CR>", desc = "Jump to Definition" },
-      { "gD",    "<Cmd>lua vim.lsp.buf.declaration()<CR>", desc = "Jump to Declaration" },
-      { "gi",    "<Cmd>lua vim.lsp.buf.implementation()<CR>", desc = "Jump to Implementation" },
-      { "gt",    "<Cmd>lua vim.lsp.buf.type_definition()<CR>", desc = "Jump to Type Definition" },
-      { "gr",    "<Cmd>lua vim.lsp.buf.references()<CR>", desc = "Jump to references" },
-      { "<C-k>", "<Cmd>lua vim.lsp.buf.signature_help()<CR>", desc = "Signature Help" },
-      { "gl",    "<Cmd>lua vim.diagnostic.open_float()<CR>", desc = "Open float" },
+      { "gr", "<Cmd>Telescope lsp_references<CR>",          desc = "Jump to references" },
+      { "gt", "<Cmd>lua vim.lsp.buf.type_definition()<CR>", desc = "Jump to Type Definition" },
     },
 
     config = function()
-      local lsp = require("lsp-zero")
-      lsp.preset("recommended")
+      local lsp_zero = require("lsp-zero")
+      lsp_zero.extend_lspconfig()
 
-      lsp.ensure_installed({
-        "rust_analyzer",
+      lsp_zero.on_attach(function(client, bufnr)
+        local zero = require("lsp-zero")
+
+        zero.default_keymaps({
+          buffer = buffnr,
+          exclude = {
+            "<F2>", -- Rename symbol
+            "<F4>", -- Code action
+            "go",   -- Jump to type definition
+          },
+        })
+
+        zero.buffer_autoformat()
+      end)
+
+      lsp_zero.format_on_save({
+        format_opts = {
+          async = false,
+          timeout_ms = 5000,
+        },
+        servers = {
+          ["rust_analyzer"] = { "rust" },
+        }
       })
 
-      lsp.set_preferences({
-        set_lsp_keymaps = false,
-      })
-
-      -- https://github.com/VonHeikemen/lsp-zero.nvim/discussions/5
-      local lsp_rust = lsp.build_options("rust_analyzer", {})
-
-      lsp.nvim_workspace({
-        library = vim.api.nvim_get_runtime_file('', true)
-      })
-
-      lsp.configure('nu', { force_setup = true })
-
-      lsp.setup()
-
-      require("rust-tools").setup({
-        server = lsp_rust
+      lsp_zero.set_sign_icons({
+        error = '',
+        warn = '',
+        hint = '',
+        info = ''
       })
     end
   },
@@ -122,37 +139,33 @@ return {
   {
     "glepnir/lspsaga.nvim",
 
-    dependencies = {
-      "gitsigns.nvim",
-    },
-
     event = {
       "BufNewFile",
       "BufReadPre",
     },
 
     keys = {
-      { "<Leader>rn", "<Cmd>Lspsaga rename<CR>", desc = "Rename Symbol" },
+      { "<Leader>rn", "<Cmd>Lspsaga rename<CR>",         desc = "Rename Symbol" },
 
-      { "<Leader>ca", "<Cmd>Lspsaga code_action<CR>", desc = "Code Action" },
-      { "<Leader>ca", "<Cmd>Lspsaga code_action<CR>", mode = "v", desc = "Code Action" },
+      { "<Leader>ca", "<Cmd>Lspsaga code_action<CR>",    desc = "Code Action" },
+      { "<Leader>ca", "<Cmd>Lspsaga code_action<CR>",    mode = "v",             desc = "Code Action" },
 
       { "<Leader>ci", "<cmd>Lspsaga incoming_calls<CR>", desc = "Incoming Calls" },
       { "<Leader>co", "<cmd>Lspsaga outgoing_calls<CR>", desc = "Outgoing Calls" },
 
-      { "<Leader>sl", "<Cmd>Lspsaga show_line_diagnostics<CR>", desc = "Line Diagnostics" },
-      { "<Leader>sc", "<Cmd>Lspsaga show_cursor_diagnostics<CR>", desc = "Cursor Diagnostics" },
-      { "<Leader>sb", "<Cmd>Lspsaga show_buf_diagnostics<CR>", desc = "Buffer Diagnostics" },
-
-      { "[e", "<Cmd>Lspsaga diagnostic_jump_prev<CR>", desc = "Prev Diagnostic" },
-      { "]e", "<Cmd>Lspsaga diagnostic_jump_next<CR>", desc = "Next Diagnostic" },
-
-      { "[E", function()
-        require("lspsaga.diagnostic").goto_prev({ severity = vim.diagnostic.severity.ERROR })
-      end, desc = "Prev Error" },
-      { "]E", function()
-        require("lspsaga.diagnostic").goto_next({ severity = vim.diagnostic.severity.ERROR })
-      end, desc = "Next Error" },
+      --     { "<Leader>sl", "<Cmd>Lspsaga show_line_diagnostics<CR>", desc = "Line Diagnostics" },
+      --     { "<Leader>sc", "<Cmd>Lspsaga show_cursor_diagnostics<CR>", desc = "Cursor Diagnostics" },
+      --     { "<Leader>sb", "<Cmd>Lspsaga show_buf_diagnostics<CR>", desc = "Buffer Diagnostics" },
+      --
+      --     { "[e", "<Cmd>Lspsaga diagnostic_jump_prev<CR>", desc = "Prev Diagnostic" },
+      --     { "]e", "<Cmd>Lspsaga diagnostic_jump_next<CR>", desc = "Next Diagnostic" },
+      --
+      --     { "[E", function()
+      --       require("lspsaga.diagnostic").goto_prev({ severity = vim.diagnostic.severity.ERROR })
+      --     end, desc = "Prev Error" },
+      --     { "]E", function()
+      --       require("lspsaga.diagnostic").goto_next({ severity = vim.diagnostic.severity.ERROR })
+      --     end, desc = "Next Error" },
     },
 
     opts = {
@@ -164,8 +177,7 @@ return {
         enable = false,
       },
       symbol_in_winbar = {
-        enable = true,
-        in_custom = true,
+        enable = false,
       }
     },
   },
@@ -195,7 +207,7 @@ return {
   },
 
   {
-    "Maan2003/lsp_lines.nvim",
+    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
 
     event = {
       "BufNewFile",
@@ -203,7 +215,7 @@ return {
     },
 
     config = function()
-      require("lsp_lines").setup()
+      require("lsp_lines").setup({})
 
       vim.diagnostic.config({
         virtual_text = false,
