ab310f7b65c3fc87710b2857e50036d43ddea316
Eric Hodel <drbrain@segment7.net>
2024-05-23 22:13:24 -0700

    Support neovide

--- lua/behavior.lua
+++ lua/behavior.lua
@@ -5,7 +5,13 @@ vim.g.hidden = true
 vim.g.nobackup = true
 vim.g.shortmess = "c"
 vim.g.wildmode = "list:longest"
-vim.g.history=200
-vim.g.shada="'100,<1000"
+vim.g.history = 200
+vim.g.shada = "'100,<1000"
 
 vim.api.nvim_set_keymap("t", "<D-Esc>", "<C-\\><C-n>", { noremap = true })
+
+-- Allow clipboard copy paste in neovim
+vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true })
--- lua/gui.lua
+++ lua/gui.lua
@@ -1,3 +1,33 @@
-vim.g.guifont="CaskadyiaCove_Nerd_Font_Complete_Mono_Light:12"
-vim.g.guioptions="aegir"
+vim.g.guioptions = "aegir"
 vim.g.mousehide = true
+vim.g.neovide_cursor_smooth_blink = false
+vim.g.neovide_cursor_trail_size = 0.4
+vim.g.neovide_cursor_vfx_mode = "railgun"
+vim.g.neovide_cursor_vfx_particle_curl = 0.5
+vim.g.neovide_cursor_vfx_particle_density = 20.0
+vim.g.neovide_cursor_vfx_particle_lifetime = 2.0
+vim.g.neovide_cursor_vfx_particle_phase = 5.0
+vim.g.neovide_cursor_vfx_particle_speed = 50.0
+vim.g.neovide_hide_mouse_when_typing = true
+vim.o.guicursor = "ci-cr-i-r:blinkwait500-blinkon500-blinkoff800"
+vim.o.guifont = "Monaspace Neon:h12"
+
+if vim.g.neovide then
+  vim.keymap.set('n', '<D-s>', ':w<CR>')      -- Save
+  vim.keymap.set('v', '<D-c>', '"+y')         -- Copy
+  vim.keymap.set('n', '<D-v>', '"+P')         -- Paste normal mode
+  vim.keymap.set('v', '<D-v>', '"+P')         -- Paste visual mode
+  vim.keymap.set('c', '<D-v>', '<C-R>+')      -- Paste command mode
+  vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
+
+  vim.g.neovide_scale_factor = 1.0
+  local change_scale_factor = function(delta)
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+  end
+  vim.keymap.set("n", "<C-=>", function()
+    change_scale_factor(1.25)
+  end)
+  vim.keymap.set("n", "<C-->", function()
+    change_scale_factor(1 / 1.25)
+  end)
+end
