8fae40b22fe0e1ad056f8106070e8b599910c7d9
Eric Hodel <drbrain@segment7.net>
2025-02-24 15:50:30 -0800

    Improve completion

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -5,8 +5,35 @@ return {
   { "hrsh7th/cmp-nvim-lsp-signature-help",  event = "VeryLazy", dependencies = "nvim-cmp" },
   { "hrsh7th/cmp-nvim-lua",                 event = "VeryLazy", dependencies = "nvim-cmp" },
   { "hrsh7th/cmp-path",                     event = "VeryLazy", dependencies = "nvim-cmp" },
-  { "hrsh7th/nvim-cmp",                     event = "VeryLazy" },
   { "onsails/lspkind.nvim",                 event = "VeryLazy" },
   { "rafamadriz/friendly-snippets",         event = "VeryLazy" },
   { "saadparwaiz1/cmp_luasnip",             event = "VeryLazy", dependencies = "nvim-cmp" },
+
+  {
+    "hrsh7th/nvim-cmp",
+
+    event = "InsertEnter",
+
+    config = function()
+      local cmp = require("cmp")
+
+      cmp.setup({
+        sources = {
+          {
+            name = "nvim_lsp"
+          },
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-d>"] = cmp.mapping.scroll_docs(4),
+        }),
+        snippet = {
+          expand = function(args)
+            vim.snippet.expand(args.body)
+          end,
+        },
+      })
+    end,
+  },
 }
