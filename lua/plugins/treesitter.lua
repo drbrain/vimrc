040e46eaea8c9bd40a7f41847b7bb7e0604bab24
Eric Hodel <drbrain@segment7.net>
2025-04-03 15:55:07 -0700

    Treesitter or LSP folding
    Better fold column markers

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -45,9 +45,28 @@ return {
     config = function(_, opts)
       require("nvim-treesitter.configs").setup(opts)
 
+      vim.o.foldenable = true
+      vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+      vim.o.foldlevel = 99
       vim.o.foldmethod = "expr"
-      vim.o.foldexpr = "nvim_treesitter#foldexpr()"
-      vim.o.foldenable = false
+      vim.o.foldtext = ""
+      vim.opt.fillchars:append({
+        fold = " ",
+        foldclose = "┣",
+        foldopen = "╋",
+        foldsep = "┃",
+      })
+      vim.opt.foldcolumn = "1"
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if client:supports_method("textDocument/foldingRange") then
+            local win = vim.api.nvim_get_current_win()
+            vim.wo[win][0].foldexpr = "v:lua.vim.lsp.foldexpr()"
+          end
+        end,
+      })
     end,
   },
 
